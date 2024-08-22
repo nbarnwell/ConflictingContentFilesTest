3e7fbd9f28d6bb0945cd90a0d07c7e7dc2186001
Neil Barnwell <neil.barnwell@bybox.com>
2024-08-22 15:52:51 +0100

    New project

---
+++ DisableTransitiveProjectReferencesTest/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using DisableTransitiveProjectReferencesTest;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder()
+          //.ConfigureAppConfiguration((hostContext, config) =>
+          //{
+          //    config.AddJsonFile("appsettings.json");
+          //})
+          .ConfigureServices((hostContext, services) =>
+          {
+              services.AddHostedService<TestService>();
+          })
+          .Build()
+          .RunAsync();
---
+++ DisableTransitiveProjectReferencesTest/TestService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DisableTransitiveProjectReferencesTest;
+
+public class TestService(IHost host, IServiceProvider provider, ILogger<TestService> logger, IConfiguration config) : IHostedService
+{
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Starting test...");
+        logger.LogInformation("Setting value = " + config["AAASetting"]);
+
+        await host.StopAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
