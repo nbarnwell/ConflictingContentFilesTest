942fc854859d44a3c09cba46aa588d83c3cb1632
Neil Barnwell <neil.barnwell@bybox.com>
2024-08-27 09:49:44 +0100

    Added extra reporting to console

--- ConflictingContentFilesTest/Program.cs
+++ ConflictingContentFilesTest/Program.cs
@@ -1,12 +1,29 @@
 ﻿
 using ConflictingContentFilesTest;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-await Host.CreateDefaultBuilder()
-          .ConfigureServices((hostContext, services) =>
-          {
-              services.AddHostedService<TestService>();
-          })
-          .Build()
-          .RunAsync();
+
+public static class Program
+{
+    public static string ContentRootPath;
+
+    public static IList<IConfigurationSource> ConfigSources { get; set; }
+
+    public static async Task Main()
+    {
+        await Host.CreateDefaultBuilder()
+                  .ConfigureAppConfiguration((hostContext, configBuilder) =>
+                  {
+                      ConfigSources = configBuilder.Sources;
+                  })
+                  .ConfigureServices((hostContext, services) =>
+                  {
+                      ContentRootPath = hostContext.HostingEnvironment.ContentRootPath;
+                      services.AddHostedService<TestService>();
+                  })
+                  .Build()
+                  .RunAsync();
+    }
+}
--- ConflictingContentFilesTest/TestService.cs
+++ ConflictingContentFilesTest/TestService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Memory;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -8,7 +11,56 @@ public class TestService(IHost host, IServiceProvider provider, ILogger<TestServ
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
-        logger.LogInformation("Setting value = " + config["AAASetting"]);
+        logger.LogInformation("CWD:              " + Directory.GetCurrentDirectory());
+        var assemblyLocation = Path.GetDirectoryName(GetType().Assembly.Location);
+        logger.LogInformation("AssemblyLocation: " + assemblyLocation, "appsettings.json");
+        logger.LogInformation("ContentRootPath:  " + Program.ContentRootPath);
+        logger.LogInformation("Setting value:    " + config["AAASetting"]);
+
+        var sourcesDetail = new StringBuilder().AppendLine("Config sources:");
+        foreach (var source in Program.ConfigSources)
+        {
+            sourcesDetail.AppendLine(source.GetType().Name + ": " + GetConfigSourceDetail(source));
+        }
+
+        logger.LogInformation(sourcesDetail.ToString());
+
+        var assemblyLocationAppSettingsFilePath = Path.Combine(assemblyLocation, "appsettings.json");
+        if (File.Exists(assemblyLocationAppSettingsFilePath))
+        {
+            var configFileContent = await File.ReadAllLinesAsync(assemblyLocationAppSettingsFilePath, cancellationToken);
+            logger.LogInformation("AssemblyLocation config content: " + string.Join(Environment.NewLine, configFileContent));
+        }
+        else
+        {
+            logger.LogWarning("AppSetttings file does not exist: " + assemblyLocationAppSettingsFilePath);
+        }
+
+        var currentDirectoryAppSettingsFilePath = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+        if (File.Exists(currentDirectoryAppSettingsFilePath))
+        {
+            var configFileContent = await File.ReadAllLinesAsync(currentDirectoryAppSettingsFilePath, cancellationToken);
+            logger.LogInformation("CWD config content: " + string.Join(Environment.NewLine, configFileContent));
+        }
+        else
+        {
+            logger.LogWarning("AppSetttings file does not exist: " + currentDirectoryAppSettingsFilePath);
+        }
+
+        await host.StopAsync(cancellationToken);
+    }
+
+    private string GetConfigSourceDetail(IConfigurationSource source)
+    {
+        return source switch
+        {
+            JsonConfigurationSource jsonSource => jsonSource.Path,
+            FileConfigurationSource fileSource => fileSource.Path,
+            MemoryConfigurationSource memorySource => string.Join(", ", memorySource.InitialData.Select(kv => kv.Key + "=" + kv.Value)),
+            StreamConfigurationSource streamSource => streamSource.Stream.GetType().Name,
+            IConfigurationProvider provider => provider.GetType().Name,
+            _ => source.ToString()
+        };
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
