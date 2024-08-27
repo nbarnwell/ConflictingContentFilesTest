59ed61ad192eba204f45f3dbcdfb65b8d7569418
Neil Barnwell <neil.barnwell@bybox.com>
2024-08-27 10:07:12 +0100

    Simplified example

--- ConflictingContentFilesTest/Program.cs
+++ ConflictingContentFilesTest/Program.cs
@@ -1,29 +1,19 @@
-﻿
-using ConflictingContentFilesTest;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-
+﻿namespace ConflictingContentFilesTest;
 
 public static class Program
 {
-    public static string ContentRootPath;
-
-    public static IList<IConfigurationSource> ConfigSources { get; set; }
-
     public static async Task Main()
     {
-        await Host.CreateDefaultBuilder()
-                  .ConfigureAppConfiguration((hostContext, configBuilder) =>
-                  {
-                      ConfigSources = configBuilder.Sources;
-                  })
-                  .ConfigureServices((hostContext, services) =>
-                  {
-                      ContentRootPath = hostContext.HostingEnvironment.ContentRootPath;
-                      services.AddHostedService<TestService>();
-                  })
-                  .Build()
-                  .RunAsync();
+        var assemblyLocationAppSettingsFilePath = Path.GetFullPath("appsettings.json");
+        if (File.Exists(assemblyLocationAppSettingsFilePath))
+        {
+            var configFileContent = await File.ReadAllLinesAsync(assemblyLocationAppSettingsFilePath);
+            Console.WriteLine(assemblyLocationAppSettingsFilePath + " content:");
+            Console.WriteLine(string.Join(Environment.NewLine, configFileContent));
+        }
+        else
+        {
+            Console.WriteLine("AppSetttings file does not exist");
+        }
     }
 }
--- ExternalLibrary/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ExternalLibrary
-{
-    public class Class1
-    {
-        public static string Message = "Hello from ExternalLibrary!";
-    }
-}
